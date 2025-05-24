162eb32a21bb6df822dd2ee47757f4b51fdf7efd
TiagoPaulin <thiago.vmp15@gmail.com>
2025-05-24 19:59:10 -0300

    Criação da entidade Heurística

---
+++ src/main/java/com/pandyzer/backend/repositories/HeuristicRepository.java
@@ -0,0 +1,10 @@
+package com.pandyzer.backend.repositories;
+
+import com.pandyzer.backend.models.Heuristic;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HeuristicRepository extends JpaRepository<Heuristic, Long> {
+
+
+
+}
---
+++ src/main/java/com/pandyzer/backend/services/HeuristicService.java
@@ -0,0 +1,71 @@
+package com.pandyzer.backend.services;
+
+import com.pandyzer.backend.models.Heuristic;
+import com.pandyzer.backend.repositories.HeuristicRepository;
+import com.pandyzer.backend.services.exceptions.BadRequestException;
+import com.pandyzer.backend.services.exceptions.ResourceNotFoundException;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class HeuristicService {
+
+    @Autowired
+    private HeuristicRepository repository;
+
+    public Heuristic findById (Long id) {
+
+        Optional<Heuristic> obj = repository.findById(id);
+
+        return obj.orElseThrow(() -> new ResourceNotFoundException("Heuristica", id));
+
+    }
+
+    public Heuristic insert (Heuristic obj) {
+
+        validate(obj);
+        return repository.save(obj);
+
+    }
+
+    public void delete (Long id) {
+
+        repository.deleteById(id);
+
+    }
+
+    @Transactional
+    public Heuristic update (Long id, Heuristic obj) {
+
+        validate(obj);
+        Heuristic heuristic = repository.getReferenceById(id);
+        updateData(heuristic, obj);
+        return repository.save(heuristic);
+
+    }
+
+    private void updateData(Heuristic heuristic, Heuristic obj) {
+
+        heuristic.setDescription(obj.getDescription());
+
+    }
+
+    private void validate(Heuristic heuristic) {
+
+        if (isNullOrEmptyOrBlank(heuristic.getDescription())) {
+            throw new BadRequestException("Informe uma descrição para a heurística.");
+        }
+
+    }
+
+    private boolean isNullOrEmptyOrBlank(String value) {
+
+        return value == null || value.trim().isEmpty();
+
+    }
+
+
+}
