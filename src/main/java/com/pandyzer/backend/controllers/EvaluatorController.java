508801e97613417c0916a583e6d4d1e111828d8e
TiagoPaulin <thiago.vmp15@gmail.com>
2025-05-25 16:18:41 -0300

    Entidade avaliador, muitos para muitos de usuário com avaliação

---
+++ src/main/java/com/pandyzer/backend/controllers/EvaluatorController.java
@@ -0,0 +1,54 @@
+package com.pandyzer.backend.controllers;
+
+import com.pandyzer.backend.models.Evaluator;
+import com.pandyzer.backend.services.EvaluatorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/evaluators")
+public class EvaluatorController {
+
+    @Autowired
+    private EvaluatorService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Evaluator> findById (@PathVariable Long id) {
+
+        Evaluator obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+
+    }
+
+    @PostMapping
+    public ResponseEntity<Evaluator> insert (@RequestBody Evaluator obj) {
+
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete (@PathVariable Long id) {
+
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Evaluator> update (@PathVariable Long id, @RequestBody Evaluator obj) {
+
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+
+    }
+
+
+}
