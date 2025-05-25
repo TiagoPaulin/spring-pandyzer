87910cf1681ab5534d2d499a20dcbf765de44b32
Gabriel Felipe Jess Meira <meira.gabriel@tecpuc.com.br>
2025-05-25 20:38:52 -0300

    CONFIGURAÇÃO DO CORS

---
+++ src/main/java/com/pandyzer/backend/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.pandyzer.backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*") //Depois trocar para o localhost:8081, por enquanto fica assim
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
